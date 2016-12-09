db756c1dee3ddb28782b63f68ef9338a32ac519c
unknown <>
2016-12-10 05:31:33 +0900

    h

---
+++ Jinbo/Assets/04.Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player :MonoBehaviour {
+
+	public float speed = 3;
+
+	protected Animator animator;
+	private Vector2 direction;
+
+	// NOTE: 컴퓨터에서 테스트 하기 위해 방향키로 조작하기 위해서는 false, 모바일에서 터치패드 이용 가능하게 하려면 true.
+	public bool useTouchPad = false;
+
+	void Start() {
+		animator = GetComponent<Animator>();
+	}
+
+	void Update() {
+		if (!useTouchPad) {
+			direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		}
+
+		transform.Translate(direction * Time.deltaTime);
+	}
+
+	public void setDirection(Vector2 dir)
+	{
+		direction = dir;
+	}
+
+	public Vector2 getDirection() {
+		return direction;
+	}
+
+	public Vector3 getPosition() {
+		return transform.position;
+	}
+}
---
+++ Jinbo/Assets/04.Scripts/RayCastManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCastManager :MonoBehaviour {
+
+	public string DO_SEARCH = "조사/행동";
+	public string DO_OK = "확인";
+	private bool isCancelable = false;
+
+	public Player player;
+	public ObjectManager Drag;
+
+	public void ClickDoButton() {
+		if (isCancelable) {
+			isCancelable = false;
+			Drag.onDrag = false;
+			return;
+		}
+
+		// Background 레이어를 제외한 것을 탐색.
+		RaycastHit2D hit = Physics2D.Raycast(player.getPosition(), player.getDirection(),2,10);
+		Debug.Log ("작동");
+		if (hit.collider != null) {
+			
+			Debug.Log (hit.collider.tag);
+			Do(hit.collider);
+		}
+	}
+
+	private void Do(Collider2D collider) {
+		switch (collider.tag) {
+		case "MoveAble":
+			//uiManager.setDoButtonText(DO_OK);
+			Drag.onDrag = true;
+			isCancelable = true;
+			break;
+		case "UnMoveAble":
+			//uiManager.ShowToast(true, "끌고 이동할 수 없는 물체다.");
+			//uiManager.setDoButtonText(DO_OK);
+			isCancelable = true;
+			break;
+		case "장애물":
+			break;
+		default:
+			break;
+		}
+	}
+}
