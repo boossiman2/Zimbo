382bdfcb1c00d3db9894fd20199907b3484d3241
Kim JaeYeong <wodud7308@naver.com>
2016-12-10 16:25:07 +0900

    보스몬스터,UI버튼,씬매니저

---
+++ BossMonster.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class BossMonster : MonoBehaviour
+{
+    private float coolDown = 3.0f;
+    private float currentTime = 3.0f;
+    [SerializeField]
+    private float speed = 1.5f;
+    private float jumpSpeed = 10.0f;
+
+    private bool isDash = false;
+    private bool isJump = false;
+
+    public float Speed
+    {
+        get { return speed; }
+        private set { speed = value; }
+    }
+
+    public bool IsCoolDown
+    {
+        get
+        {
+            if (currentTime <= 0) return true;
+            else return false;
+        }
+    }
+
+    private IEnumerator SpeedDown(float time)
+    {
+        speed = speed * 0.8f;
+        yield return new WaitForSeconds(time);
+        speed = 1.5f; 
+    }
+
+    public void Slow(float time)
+    {
+        StartCoroutine(SpeedDown(time));
+    }
+
+    private string RandomMotion()
+    {
+        if(!IsCoolDown)
+        {
+            return " ";
+        }
+        int type = Random.Range(1, 3);
+        Debug.Log(type);
+        if(type == 1)
+        {
+            return "Jump";
+        }
+        else
+        {
+            return "Dash";
+        }
+    }
+
+    private void WholeSkillDelay()
+    {
+        if(IsCoolDown)
+        {
+            StartCoroutine(WaitForSkills(RandomMotion()));
+        }
+        else
+        {
+            currentTime -= Time.deltaTime;
+        }
+    }
+
+    private IEnumerator WaitForSkills(string name)
+    {
+        if (name == "Dash" && isJump == false)
+        {
+            Debug.Log("Dash");
+            speed = 9;
+            isDash = true;
+            yield return new WaitForSeconds(0.3f);
+            speed = 1.5f;
+            isDash = false;
+        }
+        if ((name == "Jump" && isDash == false)|| isJump )
+        {
+            Debug.Log("Jump");
+            isJump = true;
+            transform.Translate(new Vector3(0, jumpSpeed * Time.deltaTime, 0));
+            yield return new WaitForSeconds(2.0f);
+            isJump = false;
+        }
+        currentTime = coolDown;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        WholeSkillDelay();
+        if(Input.GetKey(KeyCode.A))
+        {
+            Slow(1.0f);
+        }
+    }
+}
